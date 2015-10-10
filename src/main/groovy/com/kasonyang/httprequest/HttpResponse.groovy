f6da309086121bc8f7ed38df23d9e4f5bb69a920
kasonyang <i@kasonyang.com>
2015-10-11 00:06:34 +0800

    add dynamic method

--- src/main/groovy/com/kasonyang/httprequest/HttpResponse.groovy
+++ src/main/groovy/com/kasonyang/httprequest/HttpResponse.groovy
@@ -1,5 +1,7 @@
 package com.kasonyang.httprequest
 
+import groovy.json.JsonException
+import groovy.json.JsonSlurper
 import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.util.EntityUtils;
 /**
@@ -18,6 +20,30 @@ class HttpResponse {
 		this.response = response
 		this.statusCode = response.getStatusLine().getStatusCode()
 		this.text = EntityUtils.toString(response.getEntity())
+	}
+	
+	def getJson(){
+		if(!json){
+			def jsonSlurper = new JsonSlurper()
+			try{
+				json = jsonSlurper.parseText(text)
+			}catch(JsonException ex){
+				json = null
+			}
+		}
+		return json
+	}
+	
+	def methodMissing(String name,Object args){
+		this.response.invokeMethod(name,args)
+	}
+	
+	def propertyMissing(String name){
+		this.response."$name"
+	}
+	
+	def propertyMissing(String name,Object val){
+		this.response."$name" = val
 	}
 	
 }
